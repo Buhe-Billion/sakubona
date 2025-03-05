282733f05a3550eaa7e472fb52ccb68cbd52d476
B.Billion <buhebillion@gmail.com>
2025-03-05 18:35:59 +0200

    finito

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int
+main
+(int argc, char const *argv[])
 {
 
   printf("Guten Tag! Wie geht's? Ich hoffe alles laeuft gut bei dir! Bis dann, Servus\n");
